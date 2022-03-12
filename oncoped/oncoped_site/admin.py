44bf3b9d4044da9dee0aabd5dc4343f95ffaed46
Angel Ramboi <angel@ramboi.com>
2022-03-12 14:41:30 +0200

    fix: email template admin and initial seed (#49)

--- oncoped/oncoped_site/admin.py
+++ oncoped/oncoped_site/admin.py
@@ -6,5 +6,18 @@ from .models import EmailTemplate
 
 @admin.register(EmailTemplate)
 class AdminEmailTemplate(admin.ModelAdmin):
-    class Media:
-        js = ("ckeditor.js",)
+    def has_module_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
--- oncoped/oncoped_site/management/commands/seed_email_templates.py
+++ oncoped/oncoped_site/management/commands/seed_email_templates.py
@@ -9,9 +9,7 @@ Hi,
 The request for patient "{{ pr.first_name }} {{ pr.last_name }}" was received with the following data:
 
 Requester Type: {{ pr.get_requester_category_display }}
-{% if pr.institution_name %}
-Organization Name: {{ pr.institution_name }}
-{% endif %}
+{% if pr.institution_name %}Organization Name: {{ pr.institution_name }}{% endif %}
 Requester First Name: {{ pr.requester_first_name }}
 Requester Last Name: {{ pr.requester_last_name }}
 Requester Phone: {{ pr.requester_phone_number }}
@@ -22,7 +20,7 @@ Last Name: {{ pr.last_name }}
 Birth Date: {{ pr.birth_date }}
 Sex: {{ pr.get_sex_display }}
 
-{% if pr.known_complete_diagnostic %}
+{% if pr.known_complete_diagnostic == "1" %}
 Complete Diagnostic: {{ pr.complete_diagnostic }}
 Date Diagnosed: {{ pr.date_diagnosed }}
 Diagnosing Institution Name: {{ pr.diagnosing_institution_name }}
@@ -32,16 +30,15 @@ General Problem Description: {{ pr.general_problem_description }}
 
 What are the medical services the patient needs?
 {{ pr.get_therapy_needs_display }}
-{% if pr.other_therapy_needs %}
-Other Therapy Needs: {{ pr.other_therapy_needs }}
-{% endif %}
+
+{% if pr.other_therapy_needs %}Other Therapy Needs: {{ pr.other_therapy_needs }}{% endif %}
 
 Child Current Address: {{ pr.child_current_address }}
 Child Current City: {{ pr.child_current_city }}
 Child Current Country: {{ pr.child_current_country }}
 
 Thank you!
-Pediatric Oncology Team
+Ukraine Child Cancer Help Romania
 
 --- RO ---
 
@@ -63,7 +60,7 @@ Nume pacient: {{ pr.last_name }}
 Data de Naștere: {{ pr.birth_date }}
 Sex: {{ pr.get_sex_display }}
 
-{% if pr.known_complete_diagnostic %}
+{% if pr.known_complete_diagnostic == "1" %}
 Diagnostic Complet : {{ pr.complete_diagnostic }}
 Data de diagnostic : {{ pr.date_diagnosed }}
 Numele Instituției care a diagnosticat: {{ pr.diagnosing_institution_name }}
@@ -73,16 +70,15 @@ Descrierea problemei : {{ pr.general_problem_description }}
 
 Care sunt serviciile medicale de care are nevoie pacientului?
 {{ pr.get_therapy_needs_display }}
-{% if pr.other_therapy_needs %}
-Alte Nevoi terapeutice : {{ pr.other_therapy_needs }}
-{% endif %}
+
+{% if pr.other_therapy_needs %}Alte Nevoi terapeutice : {{ pr.other_therapy_needs }}{% endif %}
 
 Adresa Curenta a pacientului: {{ pr.child_current_address }}
 Orașul Curent al pacientului: {{ pr.child_current_city }}
 Țara curentă a pacientului: {{ pr.child_current_country }}
 
 Va multumim!
-Echipa Oncologie Pediatrica
+Ukraine Child Cancer Help Romania
 """
 
 
