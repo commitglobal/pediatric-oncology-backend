d3f31e5d451af227bc2ba0824c5ed439b10fcd82
Angel Ramboi <angel@ramboi.com>
2022-03-13 18:01:15 +0100

    update: email templates

--- oncoped/oncoped_site/models.py
+++ oncoped/oncoped_site/models.py
@@ -1,20 +1,23 @@
 from ckeditor.fields import RichTextField
 from django.db import models
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
 EMAIL_TEMPLATE_CHOICES = [
-    ("patient_request", _("Patient Request Form Email")),
+    ("patient_request", _("Patient Request Form")),
 ]
 
 
 class EmailTemplate(models.Model):
-    template = models.CharField(_("Template"), choices=EMAIL_TEMPLATE_CHOICES, max_length=50, unique=True)
+    template = models.CharField(_("Template"), choices=EMAIL_TEMPLATE_CHOICES, max_length=50)
     text_content = models.TextField(_("Text content"))
     html_content = RichTextField(_("HTML content"), blank=True)
+    lang = models.CharField(_("Language"), choices=settings.LANGUAGES, max_length=2)
 
     class Meta:
         verbose_name = _("Email template")
         verbose_name_plural = _("Email templates")
+        unique_together = ['template', 'lang']
 
     def __str__(self):
-        return self.template
+        return f"{self.template} ({self.lang})"
